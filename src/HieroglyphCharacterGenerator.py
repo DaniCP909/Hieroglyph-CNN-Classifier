f5ff10c43a9122b9aa1992683fdfb3cc6c63f048
DaniCP909 <d.corredorpadrino@gmail.com>
2024-05-29 03:20:18 +0200

    Necessary path in constructor

--- src/HieroglyphCharacterGenerator.py
+++ src/HieroglyphCharacterGenerator.py
@@ -12,13 +12,16 @@ import os
 
 class HieroglyphCharacterGenerator:
 
-    paths = {1: "/home/dcorr/Documents/Fonts/NOTO SANS EGYPTIAN HIEROGLYPHS/Noto_Sans_Egyptian_Hieroglyphs/NotoSansEgyptianHieroglyphs-Regular.ttf",
-             2: "/home/dcorr/Documents/Fonts/NEW GARDINER/NewGardiner/NewGardinerSMP.ttf"
-             }
+    paths = [ 
+        "./files/fonts/Noto_Sans_Egyptian_Hieroglyphs/NotoSansEgyptianHieroglyphs-Regular.ttf",
+        "./files/fonts/NewGardiner/NewGardinerBMP.ttf",
+            ]
     
-    ranges = {1: (0x00013000, 0x0001342E),
-              2: (0x0000E000, 0x0000E42E)
-              }
+    
+    ranges = [ 
+        (0x00013000, 0x0001342E),
+        (0x0000E000, 0x0000E42E),
+            ]
 
     start_hex_noto = 0x00013000
     end_hex_noto = 0x0001342E
@@ -28,47 +31,74 @@ class HieroglyphCharacterGenerator:
 
 
     #font 1-2, size 1-1
-    def __init__(self, font_id=1 , font_path=None, size=270, start_hex=None, end_hex=None):
-        if(font_id <= len(self.paths)):
-            self.id_selected_font = font_id  #index in sict
-        else:
-            self.id_selected_font = 1
-
-        if(None not in (font_path, start_hex, end_hex)):
-            if(os.path.exists(font_path)):
-                new_idx = len(self.paths) + 1
-                self.paths.update({new_idx: font_path})
-                self.ranges.update({new_idx: (start_hex, end_hex)})
-                self.id_selected_font = new_idx
-        self.font = ImageFont.truetype(self.paths.get(self.id_selected_font),
-                                 size, 
-                                 encoding="unic")
-        start, end = self.ranges.get(self.id_selected_font)
-        self.hex_range = [x for x in range(start, end)]
+    def __init__(self,
+                 font_path: str,
+                 start_hex,
+                 end_hex,
+                 font_size=270,
+                 ):
         
+        if(None not in (font_path, start_hex, end_hex) and start_hex <= end_hex):
+            if(os.path.exists(font_path)):
+                self.font_path = font_path
+                self.font_size = font_size
+                self.font = ImageFont.truetype(font_path,
+                                 font_size, 
+                                 encoding="unic")
+                self.start_hex, self.end_hex = (start_hex, end_hex)
+                self.hex_range = [x for x in range(self.start_hex, self.end_hex)]
+        else:
+            print("Error arguments")
+
+    def getMinFromFont(self):
+        return self.start_hex
+    
+    def getMaxFromFont(self):
+        return self.end_hex
+    
+    def getFontLength(self):
+        return self.end_hex - self.start_hex
+    
+    
 
     #def dec2hexInRange(self, decimal):
     #    if(decimal > len(self.hex_range)):
     #        return -1
     #    map_dec_hex = map(+1, self.hex_range)
     #    print(map_dec_hex)
+
+    def label2offset(self,
+                   label: int,
+                   ):
+        if(label < 0 or label > self.getMaxFromFont()):     #ojo
+            print("Error: label out of range")
+            return (-1)
+        
+        #se devuelve la posicion deseada (desde 0 hasta selected_font_max)
+        return self.getMinFromFont() + label
+        
      
-    def getHexRange(self):
+    def getFontRange(self, id_font):
         return self.hex_range
 
-    def getCharCanvasByHex(self, hex):
-        canvas = Image.new("RGB", (500,500), "black")
+    def getCharCanvasByOffset(self, offset):
+        #para font_size = 270 => tamaño canvas aprox 500x500
+        canvas = Image.new("RGB", (2 * (self.font_size + 20), 2 * (self.font_size + 20)), "black")
         draw = ImageDraw.Draw(canvas)
-        ucode = chr(hex)
-        draw.text((15,0), ucode, "white", self.font)
+        ucode = chr(offset)
+
+        if("NewGardiner" in self.font_path): text_y = 100
+        else: text_y = 0
+
+        draw.text((200,text_y), ucode, "white", self.font)
         return canvas
     
-    def getCharCanvasByDec(self, decimal):
-        return self.getCharCanvasByHex(self.dec2hexInRange(decimal))
+    def getCharCanvasByLabel(self, label):
+        return self.getCharCanvasByOffset(self.label2offset(label))
     
-    def getImageByHex(self, hex):
-        return np.array(self.getCharCanvasByHex(hex))
+    def getImageByOffset(self, offset):
+        return np.array(self.getCharCanvasByOffset(offset))
     
-    def getImageByDec(self, decimal):
-        return self.getImageByHex(self.dec2hexInRange(decimal))
+    def getImageByLabel(self, label):
+        return self.getImageByOffset(self.label2offset(label))
 
